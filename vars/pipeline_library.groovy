5f836c496618ca0e1092401cb5ac4f3006f978ba
hediane <49047327+hediane@users.noreply.github.com>
2022-04-19 13:02:04 +0200

    library

--- vars/pipeline_library.groovy
+++ vars/pipeline_library.groovy
@@ -8,9 +8,10 @@ def call (scmurl)
             }
            stage("testCheckout") {
             checkout([$class: 'GitSCM', 
-            branches: [[name: 'refs/branch/main']], 
+            branches: [[name: 'refs/heads/main']], 
             userRemoteConfigs: [[
-                //refspec: '+refs/tags/*:refs/remotes/origin/tags/*', 
+                //refspec: '+refs/tags/*:refs/remotes/origin/tags/*',
+                
                 url: 'https://github.com/hediane/teeeeeest.git']]
         ])
     }
