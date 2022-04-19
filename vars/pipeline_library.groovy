2fefd732af3fe3cab7db73eb3f7a575e799118e2
hediane <49047327+hediane@users.noreply.github.com>
2022-04-19 10:42:14 +0200

    dockerfile6

--- vars/pipeline_library.groovy
+++ vars/pipeline_library.groovy
@@ -3,7 +3,6 @@ def call (scmurl)
     node()
     {
             echo "checking out the source "
-            git branch: 'main',
             url : 'https://github.com/hediane/teeeeeest.git'
         
     }
