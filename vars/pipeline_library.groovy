005c72feccf1f9fd0be207e7b20b6587086e0cab
hediane <49047327+hediane@users.noreply.github.com>
2022-04-19 13:04:18 +0200

    library

--- vars/pipeline_library.groovy
+++ vars/pipeline_library.groovy
@@ -11,8 +11,8 @@ def call (scmurl)
             branches: [[name: 'refs/heads/main']], 
             userRemoteConfigs: [[
                 //refspec: '+refs/tags/*:refs/remotes/origin/tags/*',
-                
-                url: 'https://github.com/hediane/teeeeeest.git']]
+                url:scmurl]]
+                //url: 'https://github.com/hediane/teeeeeest.git']]
         ])
     }
     }
