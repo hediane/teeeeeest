b798fa24fe5232981f70ac6c8ec1b4a3ec0d2804
hediane <49047327+hediane@users.noreply.github.com>
2022-04-19 13:09:03 +0200

    library

--- vars/pipeline_library.groovy
+++ vars/pipeline_library.groovy
@@ -1,18 +1,18 @@
-def call (scmurl)
+def call (Map config)
 {
     node
         { 
             stage('msg')
             {
                 echo "checking out the source scmurl "
-                echo "${scmurl}"
+                echo "${config.scmurl}"
             }
            stage("testCheckout") {
             checkout([$class: 'GitSCM', 
             branches: [[name: 'refs/heads/main']], 
             userRemoteConfigs: [[
                 //refspec: '+refs/tags/*:refs/remotes/origin/tags/*',
-                url:"${scmurl}"]]
+                url:"${config.scmurl}"]]
                 //url: 'https://github.com/hediane/teeeeeest.git']]
         ])
     }
