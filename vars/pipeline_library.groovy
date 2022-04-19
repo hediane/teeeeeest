063a9eb1bf31a95cdd2ccbfdb240c92545935e90
hediane <49047327+hediane@users.noreply.github.com>
2022-04-19 10:40:33 +0200

    dockerfile6

--- vars/pipeline_library.groovy
+++ vars/pipeline_library.groovy
@@ -2,13 +2,9 @@ def call (scmurl)
 {
     node()
     {
-        stage('checkout')
-        {
             echo "checking out the source "
             git branch: 'main',
             url : "${scmurl}"
-
-        }
         
     }
 }
