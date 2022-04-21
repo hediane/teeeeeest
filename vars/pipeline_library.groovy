e0b2bdfe5a57859fbb815c8858e86002123bf050
hediane <49047327+hediane@users.noreply.github.com>
2022-04-21 11:01:26 +0200

    library

--- vars/pipeline_library.groovy
+++ vars/pipeline_library.groovy
@@ -1,21 +1,27 @@
 def call (Map config)
 {
-    pipeline
-        {  any agent
-        stages {
+    node
+        { 
             stage('msg')
             {
                 echo "checking out the source scmurl "
                 echo "${config.scmurl}"
             }
-          
+           stage("testCheckout") {
+            checkout([$class: 'GitSCM', 
+            branches: [[name: 'refs/heads/main']], 
+            userRemoteConfigs: [[
+                //refspec: '+refs/tags/*:refs/remotes/origin/tags/*',
+                url:"${config.scmurl}"]]
+                //url: 'https://github.com/hediane/teeeeeest.git']]
+        ])
+         }
             stage('location of dockerfile') 
                 {  
-                    docker build -t teeeeeest -f "${config.dockerfileLocation}". 
+                    docker build -t teeeeeest -f "${config.dockerfileLocation}",
                     echo "checking out the source dockerfile "
                     //echo "${config.dockerfileLocation}",
                 }
     }
-        }
 
 }
